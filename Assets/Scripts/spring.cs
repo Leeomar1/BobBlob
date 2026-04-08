92d2c39ddbe6d6586ae59855a1379361d858c80d
Leeomar1 <lflore9@ilstu.edu>
2026-04-07 19:49:02 -0500

    new lvels

--- Assets/Scripts/spring.cs
+++ Assets/Scripts/spring.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 
-public class spring : MonoBehaviour {
-    public float jumpForceX;
-    public float jumpForceY;
-    
-    void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log(other.gameObject.tag);
-        if (other.gameObject.CompareTag("Player")) {
-            other.gameObject.GetComponent<PlayerMove>().SetBounce(jumpForceX, jumpForceY);
-            
+public class spring : MonoBehaviour
+{
+    public float bounceForce = 20f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.rigidbody;
+
+        if (rb != null)
+        {
+            // Force a strong upward bounce using physics (not velocity)
+            rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
         }
     }
 }
